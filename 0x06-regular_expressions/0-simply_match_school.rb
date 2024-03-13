d201acbce20050217177cb2c7ffdb32dddf43b8c
Ysn23 <yy2369964@gmail.com>
2024-03-13 15:05:30 +0000

    Done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
 
-input = ARGV[0]
-
-pattern = /School/
-
-match = input.match(pattern)
-
-if match
-    puts match[0]
-else
-    puts "\n"
-end
+print ARGV[0].scan(/School/).join
