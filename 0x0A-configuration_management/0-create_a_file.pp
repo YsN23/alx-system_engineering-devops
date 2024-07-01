ac4145e9561f9d814321dd15a7ab0bd4ab376bff
Ysn23 <yy2369964@gmail.com>
2024-07-01 13:33:13 +0100

    Done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# Manage file Owner, Permession, Content...
+
+file { 'Manage File Owner/Grp/..':
+
+    path => '/tmp/school'
+    ensure => 'present'
+    mode => '0744'
+    owner => 'www-data'
+    group => 'www-data'
+    content => "I love Puppet"
+}
